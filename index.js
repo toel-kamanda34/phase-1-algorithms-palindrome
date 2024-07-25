7db2e940b5fc1e80782186f82aa5d2e2ed6aa7a0
tkamanda <toelokemwa@gmail.com>
2024-07-25 03:32:27 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+    // Converting word to lowercase
+    word = word.toLowerCase();
+  
+    // Initializing the two pointers
+    let left = 0;
+    let right = word.length - 1;
+    
+    while (left < right) {
+      if (word[left] !== word[right]) {
+        return false;
+      }
+      left++;
+      right--;
+    }
+    
+    return true;
+  
 }
 
 /* 
-  Add your pseudocode here
+   pseudocode 
+   1. Make all letters in the word lowercase
+   2. Set up two pointers: one at the start of the word, one at the end
+   3. While the start pointer hasn't met or passed the end marker:
+          a. Look at the letters where the markers are pointing
+          b. If these letters are different:
+              - The word is not a palindrome, so answer "No" (return false)
+          c. If the letters match:
+              - Move the start marker one letter to the right
+              - Move the end marker one letter to the left
+
+4. If we've checked all the letters without finding any mismatches:
+   - The word is a palindrome, so answer "Yes" (return true)
+
 */
 
 /*
-  Add written explanation of your solution here
+  written explanation of my solution 
+  I started by making all letters lowercase. Then, I compared the first and last letters. If they match, I moved inward and compared the next pair. I keep doing this until I reached the middle. If all pairs matched, it's a palindrome! If any pair didn't match, it's not. 
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +50,19 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  // Additional test 
+  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hello"));
 }
 
 module.exports = isPalindrome;
